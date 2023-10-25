af9b93a64e15eee8820f2c8943fa9bfac910b673
Pete Hunt <pete@elementl.com>
2023-10-25 01:36:08 -0400

    better typing

--- example.py
+++ example.py
@@ -4,26 +4,27 @@ from pydantic import BaseModel
 from llama_cpp import Llama
 from enum import Enum
 
-from text_to_pydantic import text_to_pydantic
+from text_to_pydantic import LLM, AIBaseModel
 
 class Person(BaseModel):
   person_name: str
   person_age: int
 
-class Response(BaseModel):
+class Response(AIBaseModel):
   people_mentioned_in_text: List[Person]
 
-llm = Llama(
-  "../llama.cpp/models/mistral-7b-instruct-v0.1.Q5_K_M.gguf",
-  n_gpu_layers=35,
-  temperature=0,
-  n_ctx=4096,
-  seed=0,
+llm = LLM(
+  llama=Llama(
+    "../llama.cpp/models/mistral-7b-instruct-v0.1.Q5_K_M.gguf",
+    n_gpu_layers=35,
+    temperature=0,
+    n_ctx=4096,
+    seed=0,
+  )
 )
 
-response = text_to_pydantic(
+response = Response.from_natural_language(
   llm,
-  Response,
   """
     i am barney rubble, my age is 57, and i have lived in bedrock for 2 years.
     my wife is betty and she has the same last name as me, and is 2 years younger than me
@@ -44,13 +45,13 @@ class EntityType(Enum):
   politician = 'politician'
   other = 'other'
 
-class ParsedWikiPage(BaseModel):
+class ParsedWikiPage(AIBaseModel):
   wiki_page_entity_name: str
   wiki_page_entity_type: EntityType
   wiki_page_summary: str
   topic_tags: List[str]
 
-response = text_to_pydantic(llm, ParsedWikiPage, """
+parsed_wiki_page = ParsedWikiPage.from_natural_language(llm, """
 Howard Walter Florey, Baron Florey OM FRS FRCP (24 September 1898 – 21 February 1968) was an Australian pharmacologist and pathologist who shared the Nobel Prize in Physiology or Medicine in 1945 with Ernst Chain and Sir Alexander Fleming for his role in the development of penicillin.
 
 Although Fleming received most of the credit for the discovery of penicillin, it was Florey and his team at the University of Oxford who made it into a useful and effective drug, ten years after Fleming had abandoned its development. They developed techniques for growing, purifying and manufacturing the drug, tested it for toxicity and efficacy on animals, and carried out the first clinical trials. In 1941, they used it to treat a police constable from Oxford. He started to recover, but subsequently died because Florey was unable, at that time, to make enough penicillin. Later trials in Britain, the United States and North Africa were highly successful.
@@ -59,8 +60,9 @@ A graduate of the University of Adelaide, Florey studied at the University of Ox
 
 Florey's discoveries are estimated to have saved over 80 million lives, and he is regarded by the Australian scientific and medical community as one of its greatest figures. Australian Prime Minister Sir Robert Menzies said, "In terms of world well-being, Florey was the most important man ever born in Australia."
 """)
-print(response)
-assert response == ParsedWikiPage(
+print(parsed_wiki_page)
+assert parsed_wiki_page.wiki_page_entity_name == 'Howard Walter Florey'
+assert parsed_wiki_page == ParsedWikiPage(
   wiki_page_entity_name='Howard Walter Florey',
   wiki_page_entity_type=EntityType.scientist,
   wiki_page_summary='Howard Walter Florey was an Australian pharmacologist and pathologist who shared the Nobel Prize in Physiology or Medicine in 1945 with Ernst Chain and Sir Alexander Fleming. He made penicillin into a useful and effective drug, ten years after it was discovered by Fleming, and carried out the first clinical trials. Florey researched many other subjects, including lysozyme, contraception and cephalosporins. He founded the Australian National University in Canberra and served as its Chancellor until his death. His discoveries are estimated to have saved over 80 million lives, and he is regarded by the Australian scientific and medical community as one of its greatest figures.',
--- text_to_pydantic/__init__.py
+++ text_to_pydantic/__init__.py
@@ -2,17 +2,28 @@ import json
 from llama_cpp import Llama, LlamaGrammar
 from .json_schema_to_grammar import SchemaConverter
 import weakref
+from typing import Self, Callable
+from pydantic import BaseModel, ConfigDict
+
+class LLM(BaseModel):
+  model_config = ConfigDict(arbitrary_types_allowed=True)
+
+  llama: Llama
+  prompt_template: Callable[[str], str] = lambda text: f"<s>[INST] Output JSON parsed from the following natural language text:\n\n{text} [/INST] "
 
 grammars = weakref.WeakKeyDictionary()
-def text_to_pydantic(llm: Llama, pydantic_model, text: str, prompt_template=lambda text: f"<s>[INST] Output JSON parsed from the following natural language text:\n\n{text} [/INST] "):
-  if pydantic_model not in grammars:
-    converter = SchemaConverter({})
-    converter.visit(pydantic_model.model_json_schema(), "")
-    grammar = LlamaGrammar.from_string(converter.format_grammar())
-    grammars[pydantic_model] = grammar
-  grammar = grammars[pydantic_model]
-  response = llm(
-    prompt_template(text),
-    grammar=grammar, max_tokens=-1
-  )
-  return pydantic_model(**json.loads(response['choices'][0]['text']))
+
+class AIBaseModel(BaseModel):
+  @classmethod
+  def from_natural_language(cls, llm: LLM, text: str) -> Self:
+    if cls not in grammars:
+      converter = SchemaConverter({})
+      converter.visit(cls.model_json_schema(), "") # type: ignore
+      grammar = LlamaGrammar.from_string(converter.format_grammar())
+      grammars[cls] = grammar
+    grammar = grammars[cls]
+    response = llm.llama(
+      llm.prompt_template(text),
+      grammar=grammar, max_tokens=-1
+    )
+    return cls(**json.loads(response['choices'][0]['text'])) # type: ignore
