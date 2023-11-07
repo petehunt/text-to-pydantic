c17944b8efa9df2bae8eddaf2edfe7cb99e2c1fb
Pete Hunt <floydophone@gmail.com>
2023-11-06 23:55:29 -0500

    simplification

--- github_issues_example.py
+++ github_issues_example.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import List
 from pydantic import BaseModel
 from llama_cpp import Llama
 from enum import Enum
@@ -7,64 +6,76 @@ import json
 from tqdm import tqdm
 import csv
 
-from text_to_pydantic import LLM, AIBaseModel
+from text_to_pydantic import text_to_pydantic
+
 
 class GithubIssueType(Enum):
-  bug = "bug"
-  feature = "feature"
-  question = "question"
-  other = "other"
+    bug = "bug"
+    feature = "feature"
+    question = "question"
+    other = "other"
+
 
 class GithubIssueQuality(Enum):
-  high_quality = "high_quality"
-  medium_quality = "medium_quality"
-  low_quality = "low_quality"
+    high_quality = "high_quality"
+    medium_quality = "medium_quality"
+    low_quality = "low_quality"
+
 
 class GithubIssueTeam(Enum):
-  frontend = "frontend"
-  core_apis = "core_apis"
-  backend_infrastructure = "backend_infrastructure"
-  other = "other"
+    frontend = "frontend"
+    core_apis = "core_apis"
+    backend_infrastructure = "backend_infrastructure"
+    other = "other"
+
 
 class GithubClassificationConfidence(Enum):
-  low = "low"
-  medium = "medium"
-  high = "high"
+    low = "low"
+    medium = "medium"
+    high = "high"
 
 
-llm = LLM(
-  llama=Llama(
+llama = Llama(
     "mistral-7b-instruct-v0.1.Q5_K_M.gguf",
     n_gpu_layers=35,
     temperature=0,
     n_ctx=4096,
     seed=0,
-  )
 )
 
-class GithubIssue(AIBaseModel):
-  github_issue_one_line_description: str
-  github_issue_type: GithubIssueType
-  github_issue_type_confidence: GithubClassificationConfidence
-  github_issue_quality: GithubIssueQuality
-  github_issue_quality_confidence: GithubClassificationConfidence
-  github_issue_triaged_team: GithubIssueTeam
-  github_issue_triaged_team_confidence: GithubClassificationConfidence
 
+class GithubIssue(BaseModel):
+    github_issue_one_line_description: str
+    github_issue_type: GithubIssueType
+    github_issue_type_confidence: GithubClassificationConfidence
+    github_issue_quality: GithubIssueQuality
+    github_issue_quality_confidence: GithubClassificationConfidence
+    github_issue_triaged_team: GithubIssueTeam
+    github_issue_triaged_team_confidence: GithubClassificationConfidence
+
+
+issues = [
+    issue
+    for issue in json.loads(Path("issues.json").read_text())
+    if "pull_request" not in issue
+]
+texts = [
+    f"GitHub issue title: {issue['title']}\n\nGitHub issue body:\n{issue['body'][:8192]}"
+    for issue in issues
+]
 
 with open("output.csv", "w") as f:
-  writer = csv.writer(f)
-  writer.writerow(["summary", "type", "quality", "team", "url"])
-  for issue in tqdm([issue for issue in json.loads(Path("issues.json").read_text()) if "pull_request" not in issue]):
-    text = json.dumps({"title": issue["title"], "body": issue["body"][:8192]})
-    print(text)
-    parsed = GithubIssue.from_natural_language(llm, text)
-    print(parsed)
-    print('-------------')
-    writer.writerow([
-      parsed.github_issue_one_line_description,
-      parsed.github_issue_type.value,
-      parsed.github_issue_quality.value,
-      parsed.github_issue_triaged_team.value,
-      issue['html_url'],
-    ])
+    writer = csv.writer(f)
+    writer.writerow(["summary", "type", "quality", "team", "url"])
+
+    for issue, parsed in zip(issues, text_to_pydantic(llama, GithubIssue, tqdm(texts))):
+        print(parsed)
+        writer.writerow(
+            [
+                parsed.github_issue_one_line_description,
+                parsed.github_issue_type.value,
+                parsed.github_issue_quality.value,
+                parsed.github_issue_triaged_team.value,
+                issue["html_url"],
+            ]
+        )
--- text_to_pydantic/__init__.py
+++ text_to_pydantic/__init__.py
@@ -1,34 +1,26 @@
 import json
 from llama_cpp import Llama, LlamaGrammar
 from .json_schema_to_grammar import SchemaConverter
-import weakref
-from typing import Callable
-from typing_extensions import Self
-from pydantic import BaseModel, ConfigDict
+from typing import TypeVar, Type, Iterable
+from pydantic import BaseModel
 
-class LLM(BaseModel):
-  model_config = ConfigDict(arbitrary_types_allowed=True)
+T = TypeVar("T", bound=BaseModel)
 
-  llama: Llama
-  # different LLMs (vicuna, llama, mistral etc) use different instruction tuning formats
-  prompt_template: Callable[[str], str] = lambda instruction: f"<s>[INST] {instruction} [/INST] "
 
-grammars = weakref.WeakKeyDictionary()
-
-class AIBaseModel(BaseModel):
-  @classmethod
-  def from_natural_language(cls, llm: LLM, text: str, instruction_template=lambda text, json_schema: f"Output JSON parsed from natural language text that adheres to the provided JSON schema.\n\nJSON schema:\n\n{json_schema()}\n\nNatural language text:\n\n{text}") -> Self:
-    if cls not in grammars:
-      converter = SchemaConverter({})
-      json_schema = cls.model_json_schema()
-      converter.visit(json_schema, "") # type: ignore
-      grammar = LlamaGrammar.from_string(converter.format_grammar())
-      grammars[cls] = grammar
-    grammar = grammars[cls]
-    response = llm.llama(
-      # the custom instruction_template lets you customize the instructions to the model for this
-      # particular instance. usually, the default instruction_template is fine
-      llm.prompt_template(instruction_template(text, lambda: json.dumps(cls.model_json_schema()))),
-      grammar=grammar, max_tokens=-1
-    )
-    return cls(**json.loads(response['choices'][0]['text'])) # type: ignore
+def text_to_pydantic(
+    llama: Llama,
+    to_model: Type[T],
+    texts: Iterable[str],
+    prompt=lambda text, json_schema: f"<s>[INST] Output JSON parsed from natural language text that adheres to the provided JSON schema.\n\nJSON schema:\n\n{json_schema}\n\nNatural language text:\n\n{text} [/INST] ",
+) -> Iterable[T]:
+    converter = SchemaConverter({})
+    json_schema = to_model.model_json_schema()
+    converter.visit(json_schema, "")  # type: ignore
+    grammar = LlamaGrammar.from_string(converter.format_grammar())
+    for text in texts:
+        response = llama(
+            prompt(text, json_schema),
+            grammar=grammar,
+            max_tokens=-1,
+        )
+        yield to_model(**json.loads(response["choices"][0]["text"]))
